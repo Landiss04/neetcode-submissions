e444655508c847e939052f48110d43e380fee5b2
Landiss04 <l.stroh04@gmail.com>
2026-06-08 14:36:59 -0400

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num,0) + 1
+        
+        for key,value in freq.items():
+            if value >= 2:
+                return key
+        return 0
